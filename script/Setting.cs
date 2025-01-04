4a252fbbb70b362093ed1f8c3647eac24a19b521
acrsinx <14795576+acrsinx@user.noreply.gitee.com>
2025-01-04 22:56:44 +0800

    [添加]新增阴影设置。

--- script/Setting.cs
+++ script/Setting.cs
@@ -5,15 +5,23 @@ public partial class Setting : Control {
     public OptionButton uiType;
     public OptionButton maxFps;
     public CheckButton useScreenShader;
+    public CheckButton shadow;
+    private Light3D light;
     public Button exit;
     public void Init() {
         uiType = GetNode<OptionButton>("PanelContainer/VBoxContainer/uiType");
         maxFps = GetNode<OptionButton>("PanelContainer/VBoxContainer/maxFps");
-        exit = GetNode<Button>("PanelContainer/VBoxContainer/exit");
         useScreenShader = GetNode<CheckButton>("PanelContainer/VBoxContainer/useScreenShader");
+        shadow = GetNode<CheckButton>("PanelContainer/VBoxContainer/shadow");
+        exit = GetNode<Button>("PanelContainer/VBoxContainer/exit");
         uiType.Selected = (int)ui.uiType;
         Engine.MaxFps = maxFps.GetItemText(maxFps.GetSelectedId()).ToInt();
         useScreenShader.ButtonPressed = ui.playerCamera.screenShader.Visible;
+        light = ui.playerCamera.GetTree().Root.GetChild<Node>(0).GetChild<Node>(0).GetChild<Light3D>(0);
+        if (light is null) {
+            ui.Log("找不到灯光。");
+        }
+        shadow.ButtonPressed = light.ShadowEnabled;
         uiType.ItemSelected += (index) => {
             ui.uiType = (UiType)index;
         };
@@ -22,6 +30,9 @@ public partial class Setting : Control {
         };
         useScreenShader.Pressed += () => {
             ui.playerCamera.screenShader.Visible = useScreenShader.ButtonPressed;
+        };
+        shadow.Pressed += () => {
+            light.ShadowEnabled = shadow.ButtonPressed;
         };
         exit.Pressed += () => {
             ui.Exit();
