72c8ec76c53e3ea438170f3127a72a88c4eda03e
acrsinx <14795576+acrsinx@user.noreply.gitee.com>
2025-01-22 20:44:24 +0800

    [添加]新增LOD设置。

--- script/Setting.cs
+++ script/Setting.cs
@@ -7,13 +7,17 @@ public partial class Setting : Control {
     public CheckButton useScreenShader;
     public CheckButton shadow;
     private Light3D light;
+    private SpinBox LOD;
     public Button exit;
     public void Init() {
+        // 获取组件
         uiType = GetNode<OptionButton>("PanelContainer/VBoxContainer/uiType");
         maxFps = GetNode<OptionButton>("PanelContainer/VBoxContainer/maxFps");
         useScreenShader = GetNode<CheckButton>("PanelContainer/VBoxContainer/useScreenShader");
         shadow = GetNode<CheckButton>("PanelContainer/VBoxContainer/shadow");
         exit = GetNode<Button>("PanelContainer/VBoxContainer/exit");
+        LOD = GetNode<SpinBox>("PanelContainer/VBoxContainer/LOD");
+        // 设置初始值
         uiType.Selected = (int)ui.uiType;
         Engine.MaxFps = maxFps.GetItemText(maxFps.GetSelectedId()).ToInt();
         useScreenShader.ButtonPressed = ui.playerCamera.screenShader.Visible;
@@ -22,6 +26,8 @@ public partial class Setting : Control {
             ui.Log("找不到灯光。");
         }
         shadow.ButtonPressed = light.ShadowEnabled;
+        LOD.Value = ui.GetTree().Root.MeshLodThreshold;
+        // 绑定事件
         uiType.ItemSelected += (index) => {
             ui.uiType = (UiType)index;
         };
@@ -33,6 +39,9 @@ public partial class Setting : Control {
         };
         shadow.Pressed += () => {
             light.ShadowEnabled = shadow.ButtonPressed;
+        };
+        LOD.ValueChanged += (value) => {
+            ui.GetTree().Root.MeshLodThreshold = (float) value;
         };
         exit.Pressed += () => {
             ui.Exit();
