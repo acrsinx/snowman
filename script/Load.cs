eb067f59136e45a4eab6df93e237b3d3dc509afa
acrsinx <14795576+acrsinx@user.noreply.gitee.com>
2025-01-30 16:08:54 +0800

    [修复]修复加载进度条显示错误，添加预渲染地图。

--- script/Load.cs
+++ script/Load.cs
@@ -2,7 +2,7 @@ using Godot;
 
 public partial class Load : Control {
     public int progress = 0;
-    public const int maxProgress = 5;
+    public const int maxProgress = 6;
     public Ui ui;
     public ProgressBar progressBar;
     public override void _Ready() {
@@ -19,12 +19,13 @@ public partial class Load : Control {
         RefreshProgress();
     }
     private void RefreshProgress() {
-        progressBar.Value = (double) progress / maxProgress;
+        progressBar.Value = progress * 100.0D / maxProgress;
     }
     private void Do() {
         switch (progress) {
             case 0:
                 Plot.camera = ui.playerCamera;
+                ui.playerCamera.map.RenderTargetUpdateMode = SubViewport.UpdateMode.Always;
                 break;
             case 1:
                 ui.playerCamera.playerCharacter = new Snowman(ui.playerCamera.player, ui.playerCamera);
@@ -36,6 +37,9 @@ public partial class Load : Control {
                 new Robot(ui.playerCamera).Position = new Vector3(5, 0, 5);
                 break;
             case 4:
+                ui.playerCamera.map.GetTexture().GetImage().SavePng("user://map.png");
+                break;
+            case 5:
                 ui.playerCamera.PlayerState = State.move;
                 Plot.Check(ui);
                 Visible = false;
