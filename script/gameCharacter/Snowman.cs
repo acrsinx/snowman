a236be88e61a029935585c2d8357f09df1783b4a
acrsinx <14795576+acrsinx@user.noreply.gitee.com>
2025-03-29 17:19:35 +0800

    [添加]添加游戏角色边移动边攻击功能。

--- script/gameCharacter/Snowman.cs
+++ script/gameCharacter/Snowman.cs
@@ -14,7 +14,9 @@ public partial class Snowman: GameCharacter {
             player.cameraManager.cameraMarker.Reparent(this, false);
             player.cameraManager.SetCameraPosition();
         } else {
-            auto = new AutoCharacterManager(this, player);
+            auto = new AutoCharacterManager(this, player) {
+                canAttackWhenMoving = true
+            };
         }
         health.MaxHealth = 1000;
         health.SetFullHealth();
